73d45fccc3eb3c60805e6f65cb256d699cec6df0
vlad37419 <85108281+vlad37419@users.noreply.github.com>
2021-06-12 00:36:03 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict"
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.nav-menu');
+const menuCloseButton = document.querySelector('.menu-close');
+
+menuButton.addEventListener("click", () => {
+  menu.classList.add("is-active");
+  menuCloseButton.classList.add("is-active");
+});
+
+menuCloseButton.addEventListener("click", () => {
+  menu.classList.remove("is-active");
+  menuCloseButton.classList.remove("is-active");
+});
